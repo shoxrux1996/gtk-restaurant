271ff87593a11363732f2cb19d749807c02882eb
Shokhrukh <shohrux.shomaxmudov@gmail.com>
2017-12-17 18:03:50 +0500

    shox

--- admin_window.c
+++ admin_window.c
@@ -37,6 +37,14 @@ struct book
   GtkWidget *entry3;
   GtkWidget *entry4;
 };
+struct menu
+{
+  GtkWidget *entry1;
+  GtkWidget *entry2;
+  GtkWidget *entry3;
+  GtkWidget *entry4;
+};
+
 
 void deleteChildren(){
   GList *children, *iter;
@@ -62,14 +70,19 @@ void show_info(gchar *inf) {
   gtk_dialog_run(GTK_DIALOG(dialog));
   gtk_widget_destroy(dialog);
 }
+
+void login(GtkWidget *widget);
+void submitLogin(GtkWidget *widget, gpointer en);
+
+void mainWind();
+void dashboard();
+void messages();
+
 void listUsers(GtkWidget *widget);
 void listOrders(GtkWidget *widget);
 void listBooks(GtkWidget *widget);
+void listMenus(GtkWidget *widget);
 
-void mainWind();
-void login(GtkWidget *widget);
-void dashboard();
-void messages();
 
 void editUser(GtkWidget *widget, gpointer i);
 void updateUser(GtkWidget *widget, gpointer user);
@@ -83,7 +96,9 @@ void editBook(GtkWidget *widget, gpointer i);
 void updateBook(GtkWidget *widget, gpointer order);
 void deleteBook(GtkWidget *widget, gpointer i);
 
-void submitLogin(GtkWidget *widget, gpointer en);
+void editMenu(GtkWidget *widget, gpointer i);
+void updateMenu(GtkWidget *widget, gpointer menu);
+void deleteMenu(GtkWidget *widget, gpointer i);
 
 
 int main(int argc, char *argv[]) {
@@ -159,7 +174,6 @@ void login(GtkWidget *widget) {
 
   gtk_widget_show_all(window);
   gtk_main();  
-
 }
 void submitLogin(GtkWidget *widget, gpointer en) {
 
@@ -176,7 +190,6 @@ void submitLogin(GtkWidget *widget, gpointer en) {
     show_info("Entered wrong credentials");
   }  
 }
-
 void dashboard(){
   deleteChildren();
   GtkWidget *label1;
@@ -210,6 +223,7 @@ void dashboard(){
   g_signal_connect(G_OBJECT(button1), "clicked", G_CALLBACK(listUsers), NULL);
   g_signal_connect(G_OBJECT(button2), "clicked", G_CALLBACK(listOrders), NULL);
   g_signal_connect(G_OBJECT(button3), "clicked", G_CALLBACK(listBooks), NULL);
+  g_signal_connect(G_OBJECT(button4), "clicked", G_CALLBACK(listMenus), NULL);
   g_signal_connect(G_OBJECT(logoutBut), "clicked", G_CALLBACK(login), NULL);
   g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
   gtk_widget_show_all(window);
@@ -687,4 +701,182 @@ void deleteBook(GtkWidget *widget, gpointer i){
   int index = ((gint) (glong) (i));
   printf("%d\n", index);
   listBooks(widget);
+}
+void listMenus(GtkWidget *widget){
+  deleteChildren();
+
+  int size = 5;
+  char status_text[10]="unchacked";
+ 
+  gtk_grid_set_column_spacing(GTK_GRID(grid), 5);
+  gtk_grid_set_row_spacing(GTK_GRID(grid), 5);
+  
+  GtkWidget *number = gtk_label_new(NULL);  
+  gtk_label_set_markup(GTK_LABEL(number), "<b>#</b>");
+  gtk_grid_attach(GTK_GRID(grid), number, 0, 1, 1, 1);
+
+  GtkWidget *name = gtk_label_new(NULL);  
+  gtk_label_set_markup(GTK_LABEL(name), "<b>User</b>");
+  gtk_grid_attach(GTK_GRID(grid), name, 1, 1, 1, 1);
+
+  GtkWidget *price = gtk_label_new(NULL);
+  gtk_label_set_markup(GTK_LABEL(price), "<b>Price</b>");
+  gtk_grid_attach(GTK_GRID(grid), price, 2, 1, 1, 1);
+
+  GtkWidget *category = gtk_label_new(NULL);
+  gtk_label_set_markup(GTK_LABEL(category), "<b>Category</b>");
+  gtk_grid_attach(GTK_GRID(grid), category, 3, 1, 1, 1);
+
+  GtkWidget *stat = gtk_label_new(NULL);
+  gtk_label_set_markup(GTK_LABEL(stat), "<b>Status</b>");
+  gtk_grid_attach(GTK_GRID(grid), stat, 4, 1, 1, 1);
+
+
+  GtkWidget *backBut;
+  backBut = gtk_button_new_with_label("Back");
+  gtk_grid_attach(GTK_GRID(grid), backBut, 6, 1, 1, 1);
+
+  for(int j = 2; j <= size; j++)
+  {
+    gchar* index;
+    index = g_strdup_printf("%d", j + 1);
+
+    GtkWidget *number = gtk_label_new(index);
+    gtk_grid_attach(GTK_GRID(grid), number, 0, j, 1, 1);
+
+    GtkWidget *name = gtk_label_new("Alexandrer Stokes");
+    gtk_grid_attach(GTK_GRID(grid), name, 1, j, 1, 1);
+
+
+    GtkWidget *price = gtk_label_new("1000");
+    gtk_grid_attach(GTK_GRID(grid), price, 2, j, 1, 1);
+
+    GtkWidget *category = gtk_label_new(index);
+    gtk_grid_attach(GTK_GRID(grid), category, 3, j, 1, 1);
+
+    int status = 2;//status of the order
+    switch(status){
+      case 0:
+        strcpy(status_text, "unchacked");
+        break;
+      case 1:
+        strcpy(status_text, "rejected");
+        break;
+      case 2:
+        strcpy(status_text, "delivered");
+        break;
+      default:  
+        strcpy(status_text, "approved");
+    }
+
+    GtkWidget *stat = gtk_label_new(status_text);
+    gtk_grid_attach(GTK_GRID(grid), stat, 4, j, 1, 1);
+
+    GtkWidget *edit = gtk_button_new_with_label("edit");
+    gtk_grid_attach(GTK_GRID(grid), edit, 5, j, 1, 1);
+
+    GtkWidget *delete = gtk_button_new_with_label("delete");
+    gtk_grid_attach(GTK_GRID(grid), delete, 6, j, 1, 1);
+
+    g_signal_connect(G_OBJECT(delete),"clicked", G_CALLBACK(deleteMenu), ((gpointer) (glong) (j+1)));
+    g_signal_connect(G_OBJECT(edit),"clicked", G_CALLBACK(editMenu), ((gpointer) (glong) (j+1)));
+  }
+ 
+  g_signal_connect(G_OBJECT(backBut), "clicked", G_CALLBACK(dashboard), NULL);
+  g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+  
+  gtk_widget_show_all(window); 
+}
+void editMenu(GtkWidget *widget, gpointer i){
+  deleteChildren();
+  int index = ((gint) (glong) (i));
+  GtkWidget *label1;
+  GtkWidget *label2;
+  GtkWidget *label3;
+  GtkWidget *label4;
+
+  label1 = gtk_label_new("User:");
+  label2 = gtk_label_new("Price:");
+  label3 = gtk_label_new("Category:");
+  label4 = gtk_label_new("Status:");
+  GtkWidget *entry1;
+  GtkWidget *entry2;
+  GtkWidget *entry3;
+  GtkWidget *entry4;
+
+  entry1 = gtk_entry_new();
+  GtkAdjustment *adjustment = gtk_adjustment_new(0,0,10000,1,0, 0);
+  entry2 = gtk_spin_button_new(GTK_ADJUSTMENT(adjustment), 1, 0);
+  entry3 = gtk_combo_box_text_new();
+  entry4 = gtk_combo_box_text_new();
+
+  for (int i = 1; i < 5; i++){
+    gtk_combo_box_text_append_text (GTK_COMBO_BOX_TEXT (entry3), g_strdup_printf("%d", i));
+  }
+
+  gtk_combo_box_text_append_text (GTK_COMBO_BOX_TEXT (entry4), "unchacked");
+  gtk_combo_box_text_append_text (GTK_COMBO_BOX_TEXT (entry4), "rejected");
+  gtk_combo_box_text_append_text (GTK_COMBO_BOX_TEXT (entry4), "delivered");
+  gtk_combo_box_text_append_text (GTK_COMBO_BOX_TEXT (entry4), "approved");
+
+
+  gtk_entry_set_width_chars (GTK_ENTRY(entry1), 60);
+  gtk_entry_set_text(GTK_ENTRY(entry1), "Shokhrukh");
+
+  gtk_entry_set_width_chars (GTK_ENTRY(entry2), 60);
+  gtk_spin_button_set_value (GTK_SPIN_BUTTON(entry2), 2600);
+
+  /* Choose to set the first row as the active one by default, from the beginning */
+  gtk_combo_box_set_wrap_width (GTK_COMBO_BOX(entry3), 60);
+  gtk_combo_box_set_active (GTK_COMBO_BOX (entry3), 1);
+
+    /* Choose to set the first row as the active one by default, from the beginning */
+  gtk_combo_box_set_wrap_width (GTK_COMBO_BOX(entry4), 60);
+  gtk_combo_box_set_active (GTK_COMBO_BOX (entry4), 2);
+
+  GtkWidget *backBut;
+  backBut = gtk_button_new_with_label("back");
+  GtkWidget *update;
+  update = gtk_button_new_with_label("update");
+
+  gtk_grid_attach(GTK_GRID(grid), label1, 1, 1, 1, 1);
+  gtk_grid_attach(GTK_GRID(grid), label2, 1, 2, 1, 1);
+  gtk_grid_attach(GTK_GRID(grid), label3, 1, 3, 1, 1);
+  gtk_grid_attach(GTK_GRID(grid), label4, 1, 4, 1, 1);
+
+  gtk_grid_attach(GTK_GRID(grid), entry1, 2, 1, 1, 1);
+  gtk_grid_attach(GTK_GRID(grid), entry2, 2, 2, 1, 1);
+  gtk_grid_attach(GTK_GRID(grid), entry3, 2, 3, 1, 1);
+  gtk_grid_attach(GTK_GRID(grid), entry4, 2, 4, 1, 1);
+  gtk_grid_attach(GTK_GRID(grid), backBut,3, 1, 1, 1);
+  gtk_grid_attach(GTK_GRID(grid), update, 2, 5, 1, 1);
+
+  struct menu or;
+  or.entry1 = entry1;
+  or.entry2 = entry2;
+  or.entry3 = entry3;
+  or.entry4 = entry4;
+
+  gtk_widget_show_all(window);
+
+  g_signal_connect(update, "clicked", G_CALLBACK(updateMenu), &or);  
+  g_signal_connect(backBut, "clicked", G_CALLBACK(listMenus), NULL);  
+  g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+  gtk_main();
+}
+void updateMenu(GtkWidget *widget, gpointer menu){
+  struct menu *or = menu;
+  printf("Name:%s\n", gtk_entry_get_text (GTK_ENTRY(or->entry1)));
+  printf("Price:%d\n", gtk_spin_button_get_value_as_int(GTK_SPIN_BUTTON(or->entry2)));
+   printf("Category:%s\n", gtk_combo_box_text_get_active_text (GTK_COMBO_BOX_TEXT(or->entry3)));
+  printf("Status:%s\n", gtk_combo_box_text_get_active_text (GTK_COMBO_BOX_TEXT(or->entry4)));
+
+  show_info("Book updated successfully");
+  listMenus(widget);
+}
+void deleteMenu(GtkWidget *widget, gpointer i){
+  show_info("Menu deleted successfuly");
+  int index = ((gint) (glong) (i));
+  printf("%d\n", index);
+  listMenus(widget);
 }
